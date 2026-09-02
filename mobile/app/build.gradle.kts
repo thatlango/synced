ea7b33890843413d968ee0f638441506e7a80930
thatlago <88454247+thatlango@users.noreply.github.com>
2026-09-02 19:54:40 +0300

    Preserve Material icons dependency alias

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.hilt.work)
