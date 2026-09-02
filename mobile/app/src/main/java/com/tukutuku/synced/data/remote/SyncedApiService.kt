98daff39f273a65d376ed71f1ca7adc53c0791eb
thatlago <88454247+thatlango@users.noreply.github.com>
2026-09-02 20:40:58 +0300

    feat(android): consume upcoming bills and personal forecast

--- mobile/app/src/main/java/com/tukutuku/synced/data/remote/SyncedApiService.kt
+++ mobile/app/src/main/java/com/tukutuku/synced/data/remote/SyncedApiService.kt
@@ -16,6 +16,9 @@ interface SyncedApiService {
     @GET("plans/current") suspend fun currentPlan(): ApiEnvelope<Plan?>
     @POST("plans") suspend fun createPlan(@Body body: CreatePlanRequest): ApiEnvelope<Plan>
 
+    @GET("bills/upcoming") suspend fun upcomingBills(@Query("days") days: Int = 30): ApiEnvelope<UpcomingBills>
+    @GET("forecasts/personal") suspend fun personalForecast(): ApiEnvelope<PersonalForecast?>
+
     @GET("baskets") suspend fun baskets(): ApiEnvelope<List<Basket>>
     @POST("baskets") suspend fun createBasket(@Body body: CreateBasketRequest): ApiEnvelope<Basket>
     @POST("baskets/{id}/contributions") suspend fun contribute(@Path("id") id: String, @Body body: ContributionRequest): ApiEnvelope<JsonElementEnvelope>
